94f7c9d3e18ad5e79a55422483195cbbf005b5e2
wahyuamirulloh <amirullohw@gmail.com>
2020-10-07 15:29:23 +0700

    Landing Page

--- index.php
+++ index.php
@@ -2,8 +2,67 @@
 <html>
 <head>
 	<title>PHP Youngbook</title>
+	<link rel="stylesheet" type="text/css" href="./assets/style/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="./assets/style/index.css">
 </head>
 <body>
-
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-light" id="navbar">
+  			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  			</button>
+  			<nav class="navbar navbar-light" id="navbar">
+ 				<a class="navbar-brand" href="#">
+    				<img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+    				<span id="navbar-text">Bootstrap</span>
+  				</a>
+			</nav>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+   				 <ul class="navbar-nav mr-auto" id="navbar">
+			        <li class="nav-item active">
+			        	<a class="nav-link" href="#">Home</a>
+			      	</li>
+			     	<li class="nav-item">
+			     		<a class="nav-link" href="#">Link</a>
+			      	</li>
+			      	<li class="nav-item">
+			        	<a class="nav-link" href="#">Link</a>
+			      	</li>
+			      	<li class="nav-item">
+			       	 <a class="nav-link" href="#">Link</a>
+			      	</li>
+			      	<li class="nav-item">
+			        	<a class="nav-link" href="#">Link</a>
+			      	</li>
+    			</ul>
+			    <form class="form-inline my-2 my-lg-0">
+			    	<a href="">
+			      		<button class="btn mr-3" type="button" id="signinbutton">Login</button>    		
+			    	</a>
+			    	<a href="">
+			      		<button class="btn" type="button" id="signupbutton">Register</button>    		
+			    	</a>
+			    </form>
+  			</div>
+		</nav>
+	</header>
+	<div class="content">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl">
+					<div id="title">
+						<h1>Youngbook</h1>
+						<h2>Meet New Friends Online.</h2>
+					</div>
+					<div id="more" class="row">
+						<button class="btn btn-outline-secondary col-4">GET STARTED</button>
+						<p class="col-8">Click Here.<br>To Meet a lot of new Friends !</p>
+					</div>
+				</div>
+				<div class="col-xl">
+					<img src="./assets/image/index-camera.png" class="w-100 mr-auto px-100" id="index-camera">
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
