4fe98bf999716ee7a18e96dffae7fb7730161b35
wahyuamirulloh <amirullohw@gmail.com>
2020-11-10 19:11:26 +0700

    little improvement

--- page/functions/marketplace_buy.php
+++ page/functions/marketplace_buy.php
@@ -10,15 +10,14 @@ $buyer_id = pick_id($conn);
 $buyer = pick_username($conn, $buyer_id);
 $seller = pick_username($conn, $seller_id);
 
-$sql_message = "INSERT INTO `messages` (`id`, `username`, `message`, `receiver`) VALUES (NULL, '$buyer', 'product_id?$product_id', '$seller')";
 $sql_transaction = "INSERT INTO `transactions` (`id`, `buyer_id`, `seller_id`, `item_id`) VALUES (NULL, '$buyer_id', '$seller_id', '$product_id')";
+$sql_message = "INSERT INTO `messages` (`id`, `username`, `message`, `receiver`) VALUES (NULL, '$buyer', 'product_id?$product_id', '$seller')";
+$sql_message_2 = "INSERT INTO `messages` (`id`, `username`, `message`, `receiver`) VALUES (NULL, '$buyer', 'Aku mau beli yang ini', '$seller')";
 
 if(mysqli_query($conn, $sql_transaction)){
-    if(mysqli_query($conn, $sql_message)){
-        header("Location: /php-youngbook/page/message/chatbox.php?username=$seller");
-    } else {
-        echo mysqli_error($conn);
-    }
+    mysqli_query($conn, $sql_message);
+    mysqli_query($conn, $sql_message_2);
+    header("Location: /php-youngbook/page/message/chatbox.php?username=$seller");
 } else {
     echo mysqli_error($conn);
 }
--- page/marketplace/bacaselengkapnya.php
+++ page/marketplace/bacaselengkapnya.php
@@ -14,15 +14,20 @@
 </head>
 <body>
     <?php 
-    require '/opt/lampp/htdocs/php-youngbook/page/components/header.php';
-    require '/opt/lampp/htdocs/php-youngbook/page/functions/mysqliconnect.php';
-    require '/opt/lampp/htdocs/php-youngbook/page/functions/marketplace_pickcategoryname.php';
-    require '/opt/lampp/htdocs/php-youngbook/page/functions/marketplace_pickname.php';
+    require '../components/header.php';
+    require '../functions/mysqliconnect.php';
+    require '../functions/marketplace_pickcategoryname.php';
+    require '../functions/marketplace_pickname.php';
+    require '../functions/marketplace_pickusername.php';
+
     $product_id = $_GET['id'];
     $sql_product = "SELECT * FROM `items` WHERE `id` = '$product_id'";
     $product = mysqli_fetch_assoc(mysqli_query($conn, $sql_product));
     $category = pickcategoryname($conn, $product['item_category']);
     $seller_name = pickname($conn, $product['seller_id']);
+    $product_id = $product['id'];
+    $seller_id = $product['seller_id'];
+    $seller_username = pick_username($conn, $seller_id);
     ?>
     <div class="header-product bg-light">
         <a href="/php-youngbook/page/marketplace/index.php" class="btn"><-</a>
@@ -30,11 +35,16 @@
     </div>
     <div class="content">
         <div class="product row bg-light m-3">
-            <img src="/php-youngbook/assets/data/marketplace_image/default.jpg" class="col-2 m-3">
+            <?php if($product['is_image_exist'] == 1){ ?>
+                <img src="/php-youngbook/assets/data/marketplace_image/<?php echo $product['image_file'] ?>.jpg" class="col-2 m-3">
+            <?php } else { ?>
+                <img src="/php-youngbook/data/marketplace_image/default.jpg" class="col-2 m-3">
+            <?php } ?>
             <div class="col-9 m-3">
                 <h2><?php echo $product['item_name'] ?></h2>
+                <!-- Mockup, Will be developed soon -->
                 <p>Rating Produk : * * * * *</p>
-                <p>Rating Penjual : * * * * ( 80% )( 4/5 pesanan dipenuhi )
+                <p>Rating Penjual : * * * * ( 80% )( 4/5 pesanan dipenuhi )</p>
             </div>
         </div>
         <div class="description m-3">
@@ -50,8 +60,8 @@
         <div class="bg-light fixed-bottom p-3">
             <h4 class="mx-3">Rp<?php echo $product['item_price'] ?></h4>
             <div class="row">
-                <div class="col-10"><a href="#" class="btn btn-primary w-100">Beli Sekarang</a></div>
-                <div class="col-2"><a href="#" class="btn btn-success">#</a></div>
+                <div class="col-10"><a href="<?php echo "/php-youngbook/page/functions/marketplace_buy.php?id=$product_id&seller=$seller_id" ?>" class="btn btn-success w-100">Beli Sekarang</a></div>
+                <div class="col-2"><a href="<?php echo "/php-youngbook/page/message/chatbox.php?username=$seller_username" ?>" class="btn btn-primary"><i class="fas fa-comments fa-1x"></i></a></div>
             </div>
         </div>
         </div>
